60dd873ce9b641d875facdaa0203586e055598e8
Pablo Henrique Correa <pablohcl93@gmail.com>
2019-02-28 12:08:43 -0300

    Initial commit

---
+++ app/src/main/java/com/example/w190227/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.w190227;
+
+import android.content.res.ColorStateList;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+    }
+}
